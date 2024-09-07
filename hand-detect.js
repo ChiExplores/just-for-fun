9d2eed52e8f39fb5dce23844b8c3dbc590812e6d
Michael Tolentino <michael@3.local>
2024-09-07 13:08:45 -1000

    Added hand detection

--- hand-detect.js
+++ hand-detect.js
@@ -1,25 +1,4 @@
 const video = document.getElementById("myvideo");
-console.log('1', video);
-let video2;
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-
-  // Create a video capture (aka webcam input)
-  video2 = createCapture(VIDEO);
-  console.log('2', video2);
-
-  // Specify the resolution of the webcam input (too high and you may notice performance issues, especially if you're extracting info from it or adding filters)
-  video2.size(640, 480);
-
-  // In some browsers, you may notice that a second video appears onscreen! That's because p5js actually creates a <video> html element, which then is piped into the canvas – the added command below ensures we don't see it :)
-  video2.hide();
-}
-
-function draw() {
-
-  // Display the video just like an image!
-  image(video2, 0, 0);
-}
 
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
@@ -29,9 +8,6 @@ let updateNote = document.getElementById("updatenote");
 let isVideo = false;
 let model = null;
 
-// video.width = 500
-// video.height = 400
-
 const modelParams = {
   flipHorizontal: true, // flip e.g for video
   maxNumBoxes: 20, // maximum number of boxes to detect
@@ -40,13 +16,6 @@ const modelParams = {
 };
 
 function startVideo() {
-  // image(video, 0, 0);
-
-  // Specify the resolution of the webcam input (too high and you may notice performance issues, especially if you're extracting info from it or adding filters)
-  // video.size(200, 200);
-
-  // In some browsers, you may notice that a second video appears onscreen! That's because p5js actually creates a <video> html element, which then is piped into the canvas – the added command below ensures we don't see it :)
-  // video.hide();
   handTrack.startVideo(video).then(function (status) {
     console.log("video started", status);
     if (status) {
@@ -82,13 +51,6 @@ function runDetection() {
     if (isVideo) {
       requestAnimationFrame(runDetection);
     }
-  });
-}
-
-function runDetectionImage(img) {
-  model.detect(img).then((predictions) => {
-    console.log("Predictions: ", predictions);
-    model.renderPredictions(predictions, canvas, context, img);
   });
 }
 
