1653776662d5e58829a1b0392bd0841f1761e02f
Michael Tolentino <michael@3.local>
2024-09-07 13:08:03 -1000

    Test

--- hand-detect.js
+++ hand-detect.js
@@ -1,4 +1,26 @@
 const video = document.getElementById("myvideo");
+console.log('1', video);
+let video2;
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  // Create a video capture (aka webcam input)
+  video2 = createCapture(VIDEO);
+  console.log('2', video2);
+
+  // Specify the resolution of the webcam input (too high and you may notice performance issues, especially if you're extracting info from it or adding filters)
+  video2.size(640, 480);
+
+  // In some browsers, you may notice that a second video appears onscreen! That's because p5js actually creates a <video> html element, which then is piped into the canvas – the added command below ensures we don't see it :)
+  video2.hide();
+}
+
+function draw() {
+
+  // Display the video just like an image!
+  image(video2, 0, 0);
+}
+
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 let trackButton = document.getElementById("trackbutton");
@@ -74,7 +96,7 @@ function runDetectionImage(img) {
 handTrack.load(modelParams).then((lmodel) => {
   // detect objects in the image.
   model = lmodel;
-  console.log(model);
+  console.log('model', model);
   updateNote.innerText = "Loaded Model!";
   trackButton.disabled = false;
 });
