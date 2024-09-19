a91a83108a2b81e7b30e0608e58777634b86b8ba
chiexplores <contact@chihuynh.me>
2024-09-18 17:12:05 -1000

    bird particle

---
+++ scripts/particles/Bird.js
@@ -0,0 +1,29 @@
+class Bird {
+  constructor(x, y) {
+    this.x = random(width * 0.25, width * 0.75);
+    this.y = random(height * 0.25, height * 0.75);
+  }
+
+  fly() {
+    this.x = this.x + 0.5;
+    this.y = this.y + -0.2;
+  }
+
+  display() {
+    {
+      fill(254, 32, 32);
+      stroke(254, 32, 32)
+      triangle(this.x, this.y, this.x + 5, this.y + 11, this.x + 5, this.y - 10)
+      ellipse(this.x, this.y, 12, 3)
+    }
+  }
+}
+
+let birds = [];
+
+function drawBird() {
+  for (let i = 0; i < birds.length; i++) {
+    birds[i].fly();
+    birds[i].display();
+  }
+}
